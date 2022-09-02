8a660ea0bbf8f3a2d9b53dc190daa19f84c469ee
dindin-nibnib <odinbeuchat.ob@ikmail.com>
2022-09-02 16:53:06 +0200

    *chore|Création de la base de la page des calculateurs

---
+++ pages/calculateurs.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+let notes = ref<number[]>([]); // Reference to the array of numbers
+</script>
+
+<template>
+	<div>
+		<h1>Calculateurs</h1>
+		<h2>Heures</h2>
+		<hr />
+		<h2>Moyennes</h2>
+		<ul>
+			<li v-for="note in notes"></li>
+		</ul>
+	</div>
+</template>
+
+<style>
+</style>
