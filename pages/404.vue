25aa0839822dc96ae6a3ccd8335e4741b791a2a8
dindin-nibnib <odinbeuchat.ob@ikmail.com>
2022-09-03 11:46:48 +0200

    #chore|Création de la page 404

---
+++ pages/404.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>
+		<h1>(): undefined => {}</h1>
+		<p>La page que vous recherchez n'existe pas.</p>
+		<p>Cliquez <NuxtLink to="/">ici</NuxtLink> pour retourner à la page d'accueil.</p>
+	</div>
+</template>
