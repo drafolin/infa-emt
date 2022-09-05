42d0454af2b0c876354b218423e95fce2bec06b2
dindin-nibnib <odinbeuchat.ob@ikmail.com>
2022-09-05 13:28:45 +0200

    liens ajoutés

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -6,16 +6,24 @@
 		<div style="display: flex; align-items: baseline">
 			<span style="height:1em; ">
 				Made with </span>
-			<img src="/assets/icons/vuejs.svg" style="height:1em; " />
+			<NuxtLink to="https://vuejs.org/">
+				<img src="/assets/icons/vuejs.svg" style="height:1em; " />
+			</NuxtLink>
 			<span>, </span>
-			<img src="/assets/icons/nuxt.svg" style="height:1em; " />
+			<NuxtLink to="https://v3.nuxtjs.org/">
+				<img src="/assets/icons/nuxt.svg" style="height:1em; " />
+			</NuxtLink>
 			<span> and </span>
-			<img src="/assets/icons/heart.png" style="height:1em; " />
+			<NuxtLink to="https://www.bag.admin.ch/bag/fr/home.html">
+				<img src="/assets/icons/heart.png" style="height:1em; " />
+			</NuxtLink>
 			<span>.</span>
 		</div>
 		<div>
 			<span>Powered by </span>
-			<img src="/assets/icons/vercel-icon-dark.svg" style="height:1em; " />
+			<NuxtLink to="https://vercel.com/">
+				<img src="/assets/icons/vercel-icon-dark.svg" style="height:1em; " />
+			</NuxtLink>
 		</div>
 	</div>
 </template>
