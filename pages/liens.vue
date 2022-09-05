9ff0e1c05e4365fedbca34ceacf6f58189756909
dindin-nibnib <odinbeuchat.ob@ikmail.com>
2022-09-05 13:15:14 +0200

    added auto content

--- pages/liens.vue
+++ pages/liens.vue
@@ -4,6 +4,16 @@
 
 <template>
 	<div>
-
+		<h1>Liens utiles</h1>
+		<div>
+			<ul>
+				<li v-for="link in links" :key="link.key">
+					<NuxtLink :to="link.url" target="_blank">
+						<img height="12px" :src="link.icon" :alt="'icône' + link.key" />
+						{{ link.name }}
+					</NuxtLink>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
