97d354807f1b2aaa8e78686aa7dd249af7990408
dindin-nibnib <odinbeuchat.ob@ikmail.com>
2022-09-05 17:23:29 +0200

    structure d'une personne

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,14 @@
 <script lang="ts" setup>
-
+type Personne = {
+	nom: string,
+	prenom: string,
+	tel: string,
+	email: string,
+	instagram?: string,
+	discord?: string,
+	snap?: string,
+	dateNaissance: Date,
+};
 </script>
 
 <template>
