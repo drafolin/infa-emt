ac346b89a6edb6407a4878015459be4af0837619
dindin-nibnib <odinbeuchat.ob@ikmail.com>
2022-09-05 13:22:10 +0200

    Séparé la liste en deux

--- pages/liens.vue
+++ pages/liens.vue
@@ -1,17 +1,5 @@
 <script lang="ts" setup>
-const links = ref([
-	{
-		key: "VSCode",
-		name: "Installateur VSCode",
-		url: "https://code.visualstudio.com/download",
-		icon: "https://code.visualstudio.com/favicon.ico",
-	},
-	{
-		key: "Infomaniak",
-		name: "Manager infomaniak",
-		url: "https://manager.infomaniak.com/",
-		icon: "https://manager.infomaniak.com/v3/assets/favicon/favicon-16x16.png"
-	},
+const liensEcole = ref([
 	{
 		key: "Hyperplanning",
 		name: "Hyperplanning",
@@ -25,14 +13,39 @@ const links = ref([
 		icon: "https://www.ict-berufsbildung.ch/favicon.ico"
 	}
 ]);
+
+const liensExternes = ref([
+	{
+		key: "VSCode",
+		name: "Installateur VSCode",
+		url: "https://code.visualstudio.com/download",
+		icon: "https://code.visualstudio.com/favicon.ico",
+	},
+	{
+		key: "Infomaniak",
+		name: "Manager infomaniak",
+		url: "https://manager.infomaniak.com/",
+		icon: "https://manager.infomaniak.com/v3/assets/favicon/favicon-16x16.png"
+	},
+]);
 </script>
 
 <template>
 	<div>
 		<h1>Liens utiles</h1>
 		<div>
+			<h2>Liens de l'école</h2>
 			<ul>
-				<li v-for="link in links" :key="link.key">
+				<li v-for="link in liensEcole" :key="link.key">
+					<NuxtLink :to="link.url" target="_blank">
+						<img height="12px" :src="link.icon" :alt="'icône' + link.key" />
+						{{ link.name }}
+					</NuxtLink>
+				</li>
+			</ul>
+			<h2>Liens externes</h2>
+			<ul>
+				<li v-for="link in liensExternes" :key="link.key">
 					<NuxtLink :to="link.url" target="_blank">
 						<img height="12px" :src="link.icon" :alt="'icône' + link.key" />
 						{{ link.name }}
