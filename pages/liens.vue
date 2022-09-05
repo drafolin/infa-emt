c1ba73686a8cc71d26d62828ccd94478971790db
dindin-nibnib <odinbeuchat.ob@ikmail.com>
2022-09-05 13:15:22 +0200

    populated page

--- pages/liens.vue
+++ pages/liens.vue
@@ -1,5 +1,30 @@
 <script lang="ts" setup>
-
+const links = ref([
+	{
+		key: "VSCode",
+		name: "Installateur VSCode",
+		url: "https://code.visualstudio.com/download",
+		icon: "https://code.visualstudio.com/favicon.ico",
+	},
+	{
+		key: "Infomaniak",
+		name: "Manager infomaniak",
+		url: "https://manager.infomaniak.com/",
+		icon: "https://manager.infomaniak.com/v3/assets/favicon/favicon-16x16.png"
+	},
+	{
+		key: "Hyperplanning",
+		name: "Hyperplanning",
+		url: "https://planning.divtec.ch/hp",
+		icon: "https://cas2.divtec.ch:4443/cas/favicon.ico"
+	},
+	{
+		key: "ModulBaukasten",
+		name: "Plan modulaire",
+		url: "https://www.ict-berufsbildung.ch/services/modulbaukasten",
+		icon: "https://www.ict-berufsbildung.ch/favicon.ico"
+	}
+]);
 </script>
 
 <template>
