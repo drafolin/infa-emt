441cdf065e04146153155652078320aa79f71265
dindin-nibnib <odinbeuchat.ob@ikmail.com>
2022-09-06 08:59:40 +0200

    ajout du lien vers contact

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,9 +2,20 @@
 	<div>
 		<NuxtLink to="/"><img height="40px" src="~/assets/icons/infa.svg" alt="Logo de la classe" /></NuxtLink>
 		<nav>
-			<NuxtLink to="/calculateurs">Calculateurs</NuxtLink>
-			<NuxtLink to="/balai">Balai</NuxtLink>
-			<NuxtLink to="/liens">Liens utiles</NuxtLink>
+			<ul>
+				<li>
+					<NuxtLink to="/calculateurs">Calculateurs</NuxtLink>
+				</li>
+				<li>
+					<NuxtLink to="/balai">Balai</NuxtLink>
+				</li>
+				<li>
+					<NuxtLink to="/liens">Liens utiles</NuxtLink>
+				</li>
+				<li>
+					<NuxtLink to="/contact"></NuxtLink>
+				</li>
+			</ul>
 		</nav>
 	</div>
 </template>
