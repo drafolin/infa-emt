18c2a96b7f81ab8883797cb9c44ff1d36904d42c
dindin-nibnib <odinbeuchat.ob@ikmail.com>
2022-09-06 08:58:07 +0200

    supprimé accueil

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,7 +2,6 @@
 	<div>
 		<NuxtLink to="/"><img height="40px" src="~/assets/icons/infa.svg" alt="Logo de la classe" /></NuxtLink>
 		<nav>
-			Accueil
 			<NuxtLink to="/calculateurs">Calculateurs</NuxtLink>
 			<NuxtLink to="/balai">Balai</NuxtLink>
 			<NuxtLink to="/liens">Liens utiles</NuxtLink>
