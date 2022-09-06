a80ea644c65cd8b8cbc59cff005fbdeb8bbc6dff
dindin-nibnib <odinbeuchat.ob@ikmail.com>
2022-09-06 08:57:43 +0200

    ajout de l'icône

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,8 +1,8 @@
 <template>
 	<div>
-		Inf2A
+		<NuxtLink to="/"><img height="40px" src="~/assets/icons/infa.svg" alt="Logo de la classe" /></NuxtLink>
 		<nav>
-			<NuxtLink to="/">Accueil</NuxtLink>
+			Accueil
 			<NuxtLink to="/calculateurs">Calculateurs</NuxtLink>
 			<NuxtLink to="/balai">Balai</NuxtLink>
 			<NuxtLink to="/liens">Liens utiles</NuxtLink>
