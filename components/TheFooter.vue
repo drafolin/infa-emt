a8fe0a0eaa132ea9e2fac9706babdba5e2356efe
Odin Beuchat <odin.beuchat@divtec.ch>
2022-11-02 06:50:58 +0100

    extracted style to stylesheet

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -5,25 +5,25 @@
 		</p>
 		<div class="resources">
 			<div>
-				<span style="height:1em; ">
+				<span>
 					Made with </span>
 				<NuxtLink to="https://vuejs.org/">
-					<img src="/assets/icons/vuejs.svg" style="height:1em; " />
+					<img src="/assets/icons/vuejs.svg" />
 				</NuxtLink>
 				<span>, </span>
 				<NuxtLink to="https://v3.nuxtjs.org/">
-					<img src="/assets/icons/nuxt.svg" style="height:1em; " />
+					<img src="/assets/icons/nuxt.svg" />
 				</NuxtLink>
 				<span> and </span>
 				<NuxtLink to="https://www.bag.admin.ch/bag/fr/home.html">
-					<img src="/assets/icons/heart.png" style="height:1em; " />
+					<img src="/assets/icons/heart.png" />
 				</NuxtLink>
 				<span>.</span>
 			</div>
 			<div>
 				<span>Powered by </span>
 				<NuxtLink to="https://vercel.com/">
-					<img src="/assets/icons/vercel-icon-dark.svg" style="height:1em; " />
+					<img src="/assets/icons/vercel-icon-dark.svg" />
 				</NuxtLink>
 			</div>
 		</div>
@@ -63,6 +63,11 @@
 
 			img {
 				margin: 0 5px;
+			}
+
+			span,
+			img {
+				height: 1em;
 			}
 		}
 	}
