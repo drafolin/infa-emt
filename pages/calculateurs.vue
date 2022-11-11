de2b22e50896d0803557613a3cc1df5014936c15
Odin Beuchat <odin.beuchat@divtec.ch>
2022-11-11 09:03:50 +0100

    Restyled shit in calculators

--- pages/calculateurs.vue
+++ pages/calculateurs.vue
@@ -78,108 +78,148 @@ const removeNote = (index: number) => { // Créé une fonction removeNote
 
 <template>
 	<div>
-		<h1>Calculateurs</h1>
-		<h2>Heures</h2>
-		<div>
-			<!--h3>Heures actuelles</h3>
-			<Datepicker timePicker v-model="heuresBase" textInput autoApply :clearable="false" /-->
-			<div class="horizontal">
-				<div>
-					<h3>Arivée</h3>
-					<Datepicker timePicker v-model="heureArrivee" textInput autoApply :clearable="false" />
+		<div class="section">
+			<div class="row row-1">
+				<div class="column">
+					<h1>Calculateurs</h1>
 				</div>
-
-				<div>
-					<h3>Pause</h3>
-					<div class="horizontal">
-						<div>
-							<h4>Début</h4>
-							<Datepicker timePicker v-model="heureDebutPause" textInput autoApply :clearable="false" />
+			</div>
+		</div>
+		<div class="section">
+			<div class="row row-1">
+				<div class="column">
+					<h2>Heures</h2>
+				</div>
+			</div>
+			<div class="row row-1_2">
+				<div class="column">
+					<div>
+						<div class="align-bottom">
+							<div>
+								<h3>Arivée</h3>
+								<Datepicker timePicker v-model="heureArrivee" textInput autoApply :clearable="false" />
+							</div>
+							<div class="horizontal full-width">
+								<h3>Pause</h3>
+								<div>
+									<div>
+										<h4>Début</h4>
+										<Datepicker timePicker v-model="heureDebutPause" textInput autoApply :clearable="false" />
+									</div>
+									<div>
+										<h4>Fin</h4>
+										<Datepicker timePicker v-model="heureFinPause" textInput autoApply :clearable="false" />
+									</div>
+								</div>
+							</div>
+							<div>
+								<h3>Départ</h3>
+								<Datepicker timePicker v-model="heureDepart" textInput autoApply :clearable="false" />
+							</div>
 						</div>
 						<div>
-							<h4>Fin</h4>
-							<Datepicker timePicker v-model="heureFinPause" textInput autoApply :clearable="false" />
+							<label>
+								<input type="checkbox" v-model="pauseApresMidi">
+								Pause de l'après-midi
+							</label>
 						</div>
 					</div>
 				</div>
-
-				<div>
-					<h3>Départ</h3>
-					<Datepicker timePicker v-model="heureDepart" textInput autoApply :clearable="false" />
+				<hr>
+				<div class="column resultat">
+					<h3>Resultat</h3>
+					<h4>Travail aujourd'hui</h4>
+					<p>{{ total / 60 >= 0 ? Math.floor(total / 60) : Math.ceil(total / 60) }}h {{ total % 60 }}min</p>
+					<div class="center">
+						<h5>Matin</h5>
+						<p>{{ totalMatin / 60 >= 0 ? Math.floor(totalMatin / 60) : Math.ceil(totalMatin / 60) }}h {{ totalMatin
+								% 60
+						}}min</p>
+					</div>
+					<div>
+						<h5>Après-midi</h5>
+						<p>{{ totalApresMidi / 60 >= 0 ? Math.floor(totalApresMidi / 60) : Math.ceil(totalApresMidi / 60) }}h {{
+								totalApresMidi % 60
+						}}min</p>
+					</div>
+					<h4>{{ texteTempsRelatif }}</h4>
+					<p>{{ Math.abs((total - MINUTES_TRAVAIL_MINIMUM) / 60 >= 0
+							? Math.floor((total - MINUTES_TRAVAIL_MINIMUM) / 60)
+							: Math.ceil((total - MINUTES_TRAVAIL_MINIMUM) / 60))
+					}}h {{ Math.abs((total - MINUTES_TRAVAIL_MINIMUM) % 60) }}min</p>
 				</div>
 			</div>
+			<hr />
+
+			<h2>Moyennes</h2>
 			<div>
-				<label>
-					<input type="checkbox" v-model="pauseApresMidi">
-					Pause de l'après-midi
-				</label>
+				<input type="text" placeholder="Note" v-model="noteText"> -
+				<input type="text" placeholder="Coefficient" v-model="coeffText">%
+				<button @click="addNote">Ajouter</button>
+				<ul>
+					<li v-for="note, index in notes">
+						<button @click="() => { removeNote(index); }">X</button>
+						<!--/* Utilisation d'une fonction anonyme pour passer un paramètre*/-->
+						{{ note.note }} - {{ note.coefficient }}%
+					</li>
+				</ul>
+				<span>{{ moyenne }}</span>
 			</div>
-			<hr>
-			<h3>Resultat</h3>
-			<h4>Travail aujourd'hui</h4>
-			<p>{{ total / 60 >= 0 ? Math.floor(total / 60) : Math.ceil(total / 60) }}h {{ total % 60 }}min</p>
-			<div class="horizontal">
-				<div>
-					<h5>Matin</h5>
-					<p>{{ totalMatin / 60 >= 0 ? Math.floor(totalMatin / 60) : Math.ceil(totalMatin / 60) }}h {{ totalMatin % 60
-					}}min</p>
-				</div>
-				<div>
-					<h5>Après-midi</h5>
-					<p>{{ totalApresMidi / 60 >= 0 ? Math.floor(totalApresMidi / 60) : Math.ceil(totalApresMidi / 60) }}h {{
-							totalApresMidi % 60
-					}}min</p>
-				</div>
-			</div>
-			<h4>{{ texteTempsRelatif }}</h4>
-			<p>{{ Math.abs((total - MINUTES_TRAVAIL_MINIMUM) / 60 >= 0
-					? Math.floor((total - MINUTES_TRAVAIL_MINIMUM) / 60)
-					: Math.ceil((total - MINUTES_TRAVAIL_MINIMUM) / 60))
-			}}h {{ Math.abs((total - MINUTES_TRAVAIL_MINIMUM) % 60) }}min</p>
-		</div>
-		<hr />
-
-		<h2>Moyennes</h2>
-		<div>
-			<input type="text" placeholder="Note" v-model="noteText"> -
-			<input type="text" placeholder="Coefficient" v-model="coeffText">%
-			<button @click="addNote">Ajouter</button>
-			<ul>
-				<li v-for="note, index in notes">
-					<button @click="() => { removeNote(index); }">X</button>
-					<!--/* Utilisation d'une fonction anonyme pour passer un paramètre*/-->
-					{{ note.note }} - {{ note.coefficient }}%
-				</li>
-			</ul>
-			<span>{{ moyenne }}</span>
 		</div>
 	</div>
 </template>
 
-<style>
+<style scoped lang="scss">
+.full-width {
+	width: 100%;
+}
+
 .horizontal {
+	&.full-width {
+		:not(div):not(.dp__main *) {
+			margin-left: 10px;
+			margin-right: 10px;
+		}
+
+		div {
+			width: 100%;
+			margin-left: 0;
+			margin-right: 0;
+		}
+
+		justify-content: space-between;
+	}
+
 	display: flex;
 	flex-direction: row;
 	column-gap: 10px;
 	align-items: center;
 }
 
-h1+h2+div div:not(.horizontal) {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-}
-
 h1+h2+div {
 	max-width: max(calc(100%/3), 500px);
+
+	div:not(.horizontal) {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	>.horizontal {
+		>div {
+			border: solid 1px #8080803d;
+			height: 160px;
+		}
+	}
 }
 
-h3 {
-	text-align: center;
+.align-bottom {
+	align-items: flex-end;
 }
 
-h1+h2+div>.horizontal>div {
-	border: solid 1px #8080803d;
-	height: 160px;
+.resultat {
+	p {
+		font-size: 16px;
+	}
 }
 </style>
